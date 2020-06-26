3b259952b35788a31f1f69ad502cba0df83a85f8
LuisPeMoraRod <luismorarod98@gmail.com>
2020-06-26 09:37:26 -0600

    Add simple REST API

---
+++ src/com/Datos1/Proyecto2/CookTime/RestAPI/RecipesResources.java
@@ -0,0 +1,30 @@
+package com.Datos1.Proyecto2.CookTime.RestAPI;
+
+import java.util.ArrayList;
+import java.util.List;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+
+@Path("recipes")
+public class RecipesResources {
+	private static List<String> DB = new ArrayList<>();
+	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<String> getRecipes(){
+		return DB;
+	}
+	
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response addRecipe(String recipeName) {
+		DB.add(recipeName);
+		return Response.ok().build();
+	}
+	
+}
---
+++ src/com/Datos1/Proyecto2/CookTime/RestAPI/RestApplication.java
@@ -0,0 +1,19 @@
+package com.Datos1.Proyecto2.CookTime.RestAPI;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+@ApplicationPath("rest/")
+public class RestApplication extends Application{
+	
+	@Override
+	public Set<Class<?>> getClasses(){
+		Set<Class<?>> classList = new HashSet<>();
+		classList.add(RecipesResources.class);
+		return classList;
+	}
+
+}
